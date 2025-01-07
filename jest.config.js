1da0dceb2a9d8e5ae8aba457ec6670e870858d27
Ahmed_Elzayat <113038956+Ahmed-Elzayat112@users.noreply.github.com>
2025-01-07 12:56:17 +0200

    chore: :art: Update configs of package.json and jest

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,11 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
+  moduleNameMapper: {
+    "^@src/(.*)$": "<rootDir>/src/$1",
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   testEnvironment: "node",
   transform: {
-    "^.+.tsx?$": ["ts-jest",{}],
+    "^.+.tsx?$": ["ts-jest", {}],
   },
 };
